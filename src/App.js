cb236892c2c67a1edc3c4cf3760e149d7f4579c0
John Leintz <johnleintz@scaledflow.com>
2020-09-18 18:36:44 -0500

    added sample code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import './App.css';
+// useReducer is similar to redux
+// useReducer is like useState but 
+// typically will use objects rather than values
+// https://www.youtube.com/watch?v=kK_Wqx3RnHk
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+import React, { useState, useReducer } from 'react';
+
+import Todo from './Todo.js';
+
+export const ACTIONS = {
+    ADD_TODO: 'add-todo',
+    TOGGLE_TODO: 'toggle-todo',
+    DELETE_TODO: 'delete-todo'
+  }
+
+function reducer(todos, action) {
+  switch (action.type) {
+
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete }
+        }
+        return todo
+      })
+
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+    default:
+      return todos
+
+  }
+}
+  
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false }
 }
 
-export default App;
+export default function App() {
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('');
+
+function handleSubmit(e) {
+  // prevents page from refreshing
+  e.preventDefault();
+  dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name} })
+  setName('')
+}
+
+
+  console.log(todos);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={name} onChange={e => setName (e.target.value)}/>
+      </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      })}
+    </>
+  )
+}
